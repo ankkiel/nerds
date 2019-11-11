3e9c81a9bd0e7d2e3e905c39adc2323d5cb2253f
Anastasia <anastasia_ermolova@bk.ru>
2019-11-11 19:22:47 +0300

    Действия с попапом (js)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+var openPopup = document.querySelector(".map-information__button");
+var popup = document.querySelector(".popup");
+var closePopup = document.querySelector(".popup__close");
+var formPopup = document.querySelector(".popup__form");
+var namePopup = document.querySelector("[name=name]");
+var emailPopup = document.querySelector("[name=email]");
+var inputPopup = document.querySelectorAll(".popup__input, .popup__textarea");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+window.addEventListener("keydown", evt => {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("popup--open")) {
+      popup.classList.remove("popup--open");
+    }
+  }
+});
+openPopup.addEventListener("click", evt => {
+  evt.preventDefault();
+  popup.classList.add("popup--open");
+  popup.classList.add("popup--open--animation");
+  setTimeout(() => {
+    popup.classList.remove("popup--open--animation");
+  }, 800);
+
+  if (storage) {
+    namePopup.value = storage;
+  } else if (!storage) {
+    namePopup.focus();
+  } else {
+    emailPopup.focus();
+  }
+});
+closePopup.addEventListener("click", evt => {
+  evt.preventDefault();
+  popup.classList.remove("popup--open");
+});
+formPopup.addEventListener("submit", evt => {
+  if (!namePopup.value || !inputPopup.value) {
+    evt.preventDefault();
+    popup.classList.add("popup--error");
+    setTimeout(() => {
+      popup.classList.remove("popup--error");
+    }, 800);
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", namePopup.value);
+    }
+  }
+
+});
 ymaps.ready(function () {
   var myMap = new ymaps.Map("map", {
       center: [59.938872, 30.322617],
